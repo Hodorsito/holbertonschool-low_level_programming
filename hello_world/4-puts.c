8c962ca8339716ee7c7f5ccbcd24dad5c0888776
Hodorsito <5064@holbertonschool.com>
2022-06-06 07:23:40 -0700

    task.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point 
- * "Programming is like building a multilingual puzzle"
+ * 
  * Return: Always 0 (Success)
  */
-int main(void)
+int main("Programming is like building a multilingual puzzle")
 {
 	return (0);
 }
