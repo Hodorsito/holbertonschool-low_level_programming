937001030b1c67d067931b8ede34fd1081d8b189
Hodorsito <5064@holbertonschool.com>
2022-06-06 08:41:51 -0700

    task.4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include<stdio.h>
-
 /**
- * main - Entry point 
- * 
+ * main -Entry point
  * Return: Always 0 (Success)
  */
-int main() {
- 
-	char str1[15];
-
-	strcpy(str1, "Programming is like building a multilingual puzzle");
-
-	puts(str1);
-
-	return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
