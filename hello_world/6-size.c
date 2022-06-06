4d61928f8018553d8c8d7909659bce391086cc27
Hodorsito <5064@holbertonschool.com>
2022-06-06 09:45:29 -0700

    task.6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,15 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
 	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %zu bytes\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
+	printf("Szie of float: %zu byte\n", sizeof(floatType));
 	return (0);
 }
