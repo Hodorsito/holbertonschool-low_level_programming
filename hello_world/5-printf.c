eadf03d42c6c51c0104cd62118697dbccc4c39d4
Hodorsito <5064@holbertonschool.com>
2022-06-06 09:13:51 -0700

    task.5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,11 +2,13 @@
 #include<stdlib.h>
 /**
  * main -Entry point
- * Return: Always 0 (Success)
+ * Return: Always zero (Success)
  */
 int main(void)
 {
 	const char *str1 = "with proper grammar, but the outcome is a piece of art,";
-		printf("%s \n", str1);
-		return (0);
+	
+	printf("%s \n", str1);
+	
+	return (0);
 }
