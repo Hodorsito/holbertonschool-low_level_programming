2206ac4dbaba9e5ac7cdbcecce6326c3c8ceacb5
Hodorsito <5064@holbertonschool.com>
2022-06-06 08:54:03 -0700

    task.5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	const char* str1= "with proper grammar, but the outcome is a piece of art";
+		printf("%s \n", str1);
+		
+		return (0);
+}
