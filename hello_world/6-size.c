40d69eeab03b9f261918183982774123772fbb26
Hodorsito <5064@holbertonschool.com>
2022-06-06 09:33:32 -0700

    task.6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,13 +4,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void) {
+int main(void) {
 	int intType;
 	float floatType;
 	double doubleType;
 	char charType;
-
-	// sizeof evaluate the sizde of a variable
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	printf("Size of double: %zu bytes\n", sizeof(doubleType));
