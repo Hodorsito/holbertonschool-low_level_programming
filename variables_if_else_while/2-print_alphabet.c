03af69e9a6caa50c9f9b26266359f2e94c31db78
Hodorsito <5064@holbertonschool.com>
2022-06-07 14:17:33 -0700

    task2.2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	putchar(ch);
+
+	return (0);
+}
