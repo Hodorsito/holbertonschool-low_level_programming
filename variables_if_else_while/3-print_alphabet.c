029dbcf949bcc3269271638b3f25c7823bbbae7a
Hodorsito <5064@holbertonschool.com>
2022-06-07 15:50:55 -0700

    task2.3

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+        char ch;
+	char f;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (ch = 'A'; ch <= 'Z'; f++)
+	{
+	putchar(f);
+	}
+	putchar('\n')
+	return (0);
+}
