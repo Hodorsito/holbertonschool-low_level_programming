14198d1bc3ddae9bdc6bb924e2914fcb737eb122
Hodorsito <5064@holbertonschool.com>
2022-06-07 18:20:36 -0700

    task2.5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+	{
+		printf("%d\n", r);
+	}
+
+	return (0);
+}
