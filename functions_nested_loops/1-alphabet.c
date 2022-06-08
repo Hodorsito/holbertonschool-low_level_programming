1dbc6751c6b1815a86d4bd4c655eea8915854f05
Hodorsito <5064@holbertonschool.com>
2022-06-08 16:33:21 -0700

    task1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
+void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		_putchar(c);
