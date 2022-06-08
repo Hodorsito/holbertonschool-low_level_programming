d5ccd6613b78ba6ab139331d5bc10eda920b76ae
Hodorsito <5064@holbertonschool.com>
2022-06-08 16:24:04 -0700

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
