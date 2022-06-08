f43128e93b36c6e9bcbb92c782a231124310c252
Hodorsito <5064@holbertonschool.com>
2022-06-07 18:43:49 -0700

    task2.6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+	{
+		putchar(r);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
