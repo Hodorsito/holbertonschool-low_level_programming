a603342ebcccd807eb4a5f566db25b8cd11fb2fe
Hodorsito <5064@holbertonschool.com>
2022-06-13 13:23:27 -0700

    task8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  jack_bauer - writes the character to c to stdout
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+
+
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10)	+ '0');
+		}
+	}
+
+}
+
+
+
