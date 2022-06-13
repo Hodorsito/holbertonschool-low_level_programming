127c3e66607129eacc8852c980d011e423431905
Hodorsito <5064@holbertonschool.com>
2022-06-13 13:35:00 -0700

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,8 +14,6 @@ void jack_bauer(void)
 
 	for (h = 0; h <= 23; h++)
 	{
-
-
 		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
@@ -23,8 +21,9 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
+
 	}
 
 }
