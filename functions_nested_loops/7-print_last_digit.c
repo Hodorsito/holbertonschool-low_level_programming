f65311f5efdf54d9b06a7e143e832fcbf93a1dbd
Hodorsito <5064@holbertonschool.com>
2022-06-13 12:26:22 -0700

    task7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - writes the character to c to stdout
+ *  @c: The character to print
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	
+	if (c > 0)
+	{
+		_putchar(c + '0');
+		return (c);
+	}
+	if (c == 0)
+	{
+		_putchar(c + '0');
+		return (0);
+	}
+	if (c < 0)
+	{
+		_putchar((c * -1) + '0');
+		return (c * -1);
+	}
+
+return (0);
+}
