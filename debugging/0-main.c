09d2ea27dd105bca987ecdbf0bb7d00b0c17e62d
Hodorsito <5064@holbertonschool.com>
2022-06-14 06:12:37 -0700

    task0

---
+++ debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* positive_or_negative - tests function that prints if integer is positive or negative
+* @i: jijiji
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+        i = 98;
+        positive_or_negative(i);
+
+}
---
+++ debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* positive_or_negative - tests function that prints if integer is positive or negative
+* @i: sdfsd
+* Return: 0
+*/
+
+int positive_or_negative(int i)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+}
