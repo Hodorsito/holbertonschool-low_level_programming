006bbd5a54759770d7a7adac2636fe4542ff4951
Hodorsito <5064@holbertonschool.com>
2022-06-13 19:59:22 -0700

    task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_to_98 - writes the character to c to stdout
+ *
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+	}
+	else if (n > 98)
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	}
+	printf("98\n");
+}
