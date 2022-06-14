81552594144842c8c6950c50542a7cf54f716c53
Hodorsito <5064@holbertonschool.com>
2022-06-13 19:21:50 -0700

    task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  add - writes the character to c to stdout
+ *  @z: asjdjasd
+ *  @x: akjsdkajsd
+ *  Return: On success 1.
+ *  On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int x, int z)
+{
+	return (x + z);
+
+}
